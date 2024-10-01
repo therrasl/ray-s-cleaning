ac95edc1ddeb6ce85631d01084a3e964ef79b2aa
therrasl <164066143+therrasl@users.noreply.github.com>
2024-10-01 16:51:34 +0300

    fix adaptive

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,13 @@
 import { provideAnimations } from "@angular/platform-browser/animations";
 
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
-
+import { provideRouter } from "@angular/router";
+import { routes } from "./app.routes";
 
 
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideAnimations()
+  providers: [provideAnimations() , provideRouter(routes)
 
   ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,2 +1,6 @@
+import { Routes } from '@angular/router';
+import { MainPageComponent } from './core/pages/main-page/main-page.component';
 
-
+export const routes : Routes = [{
+  path : '' , component: MainPageComponent
+} ,]
